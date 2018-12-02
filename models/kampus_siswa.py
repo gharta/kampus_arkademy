78dc123ee5a46f29f9e3ad4bc7011831bc49d6b7
ejabu <muhammadfahreza@gmail.com>
2018-12-02 07:53:31 +0700

    Tambahan Detail Siswa

--- models/kampus_siswa.py
+++ models/kampus_siswa.py
@@ -8,3 +8,17 @@ class KampusSiswa(models.Model):
     nomor_induk = fields.Char('Nomor Induk', required=True, )
     alamat = fields.Text('Alamat')
     tanggal_masuk = fields.Date(string='Tanggal Masuk', default=fields.Date.today(), )
+
+    state = fields.Selection([
+            ('aktif', 'Aktif'),
+            ('postpone', 'Postpone'),
+            ('lulus', 'Lulus'),
+        ], string='Status', default='aktif')
+
+    # Data Pribadi
+    gender = fields.Selection([
+            ('pria', 'Pria'),
+            ('wanita', 'Wanita'),
+        ], string='Gender',)
+
+    tanggal_lahir = fields.Date(string='Tanggal Lahir', default=fields.Date.today())
